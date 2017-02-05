8bef546b50308d358b1e0a7224bf986e090108ad
Aditya Sanghi <aditya.sanghi@risingsuntech.net>
2017-02-05 12:11:49 +0530

    jQuery fetch

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,12 @@
 $(function() {
 
-  // your code will go here
-
+  $.ajax({url: "https://www.codeschool.com/users/asanghi.json",
+          method: "GET",
+          dataType: "jsonp",
+          success: function(response){
+            $.each(response.courses.completed,function(index){
+              $("#badges").append("<div class='course'><h3>"+this.title+"</h3><img src='"+this.badge+"'/><a target='_blank' href='"+this.url+"' class='btn btn-primary'>See Course</a></div>");
+            });
+          }
+        });
 });
